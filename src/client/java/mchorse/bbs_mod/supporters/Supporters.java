5a4b0c87e1f8d3c39f0e7607b32e4470bf7171bc
McHorse <mchorselessrider@gmail.com>
2024-03-13 19:06:46 +0000

    Hardcode supporters

--- src/client/java/mchorse/bbs_mod/supporters/Supporters.java
+++ src/client/java/mchorse/bbs_mod/supporters/Supporters.java
@@ -1,10 +1,5 @@
 package mchorse.bbs_mod.supporters;
 
-import mchorse.bbs_mod.data.types.BaseType;
-import mchorse.bbs_mod.data.types.ListType;
-import mchorse.bbs_mod.data.types.MapType;
-import mchorse.bbs_mod.utils.resources.LinkUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,20 +8,124 @@ public class Supporters
 {
     private List<Supporter> supporters = new ArrayList<>();
 
-    public void setup(MapType data)
+    public void setup()
     {
-        ListType supporters = data.getList("supporters");
+        this.add(new Supporter("毫无人性的大麦纸", "https://space.bilibili.com/591074", "..."));
+        this.add(new Supporter("Afica"));
+        this.add(new Supporter("76823999"));
+        this.add(new Supporter("Silly_Sheep_"));
+        this.add(new Supporter("Rainbow Stars"));
+        this.add(new Supporter("XiaoTu_MC", "https://v.douyin.com/iNwM4XEr/", "..."));
+        this.add(new Supporter("1749907"));
+        this.add(new Supporter("一身白装小兵这个"));
+        this.add(new Supporter("Fight凡子"));
+        this.add(new Supporter("GODZILLA256", "https://space.bilibili.com/17798027", "..."));
+        this.add(new Supporter("LiteCat"));
+        this.add(new Supporter("OoooorangeClover", "https://space.bilibili.com/3546579457805118/"));
+        this.add(new Supporter("萌起MENGQI", "https://space.bilibili.com/2130239542", "..."));
+        this.add(new Supporter("M1nNa"));
+        this.add(new Supporter("ANDOG", "https://space.bilibili.com/25400883", "..."));
+        this.add(new Supporter("Adfbulin"));
+        this.add(new Supporter("Relyonovo"));
+        this.add(new Supporter("老吴鸦", "...", "..."));
+        this.add(new Supporter("一团糯米NuoMi"));
+        this.add(new Supporter("第三位者"));
+        this.add(new Supporter("望空希星", "https://space.bilibili.com/494577886", "..."));
+        this.add(new Supporter("Nobody"));
+        this.add(new Supporter("星星会说话"));
+        this.add(new Supporter("一只蟠桃peach"));
+        this.add(new Supporter("tianqo"));
+        this.add(new Supporter("暴碧Bobi"));
+        this.add(new Supporter("l1ngisout", "https://space.bilibili.com/23839756", "..."));
+        this.add(new Supporter("爆风BooM", "https://space.bilibili.com/308180216"));
+        this.add(new Supporter("Seven_Chad", "https://twitter.com/seven_chad", "..."));
+        this.add(new Supporter("Deal"));
+        this.add(new Supporter("萱萱-3z", "https://space.bilibili.com/601994215", "..."));
+        this.add(new Supporter("泥土NT_black", "..."));
+        this.add(new Supporter("零雾〇五Fogg05"));
+        this.add(new Supporter("篝火工作室"));
+        this.add(new Supporter("爱发电用户_SYwD^^^"));
+        this.add(new Supporter("0tt0258"));
+        this.add(new Supporter("凤天", "https://space.bilibili.com/22348510"));
+        this.add(new Supporter("暗影苦力怕", "https://space.bilibili.com/472637101", "..."));
+        this.add(new Supporter("Zhesheng_1818", "https://www.youtube.com/channel/UC47jJ8OEHIdbq-H1xhwUzXA"));
+        this.add(new Supporter("star_tan"));
+        this.add(new Supporter("阿狐糊涂"));
+        this.add(new Supporter("开智同学"));
+        this.add(new Supporter("绯色の白初"));
+        this.add(new Supporter("GHL"));
+        this.add(new Supporter("iamli541"));
+        this.add(new Supporter("YuanMouRen"));
+        this.add(new Supporter("PearballNeko"));
+        this.add(new Supporter("九纳森九斯达", "https://space.bilibili.com/17696710"));
+        this.add(new Supporter("超威乌龙喵", "https://space.bilibili.com/14359286"));
+        this.add(new Supporter("LeaFire焰叶", "..."));
+        this.add(new Supporter("chclmii1"));
+        this.add(new Supporter("星辰不是瓜"));
+        this.add(new Supporter("terbin", "https://www.youtube.com/channel/UCD4X2slLdD_WJxlW03JPVQQ", "..."));
+        this.add(new Supporter("Kirkus"));
+        this.add(new Supporter("Tanner Walterman"));
+        this.add(new Supporter("Rheed The Legend", "https://www.youtube.com/@rheedcraft"));
+        this.add(new Supporter("fastvincent1"));
+        this.add(new Supporter("CraZzZy Boy", "..."));
+        this.add(new Supporter("Mor14"));
+        this.add(new Supporter("B13 Pictures"));
+        this.add(new Supporter("Satorik"));
+        this.add(new Supporter("SkedgyEdgy", "https://www.youtube.com/skedgyedgy", "..."));
+        this.add(new Supporter("Mr BurNik", "https://www.youtube.com/channel/UCnmttzfQHsBw0GefMToNwpA"));
+        this.add(new Supporter("Joziah3", "https://www.youtube.com/channel/UC8cnHS3Ub_EEzuaESD8E-ow"));
+        this.add(new Supporter("CreeperGoldMC"));
+        this.add(new Supporter("Roma on youtube"));
+        this.add(new Supporter("Markus"));
+        this.add(new Supporter("JesseMauve", "https://www.youtube.com/@JesseMauve/videos"));
+        this.add(new Supporter("DrGugl"));
+        this.add(new Supporter("theFallEquinox"));
+        this.add(new Supporter("Pixe_GUM"));
+        this.add(new Supporter("RunnyHero"));
+        this.add(new Supporter("Kludge"));
+        this.add(new Supporter("seb024YT"));
+        this.add(new Supporter("良辰 薛"));
+        this.add(new Supporter("PracticeTyping"));
+        this.add(new Supporter("GEVids", "https://www.youtube.com/c/GEVids", "..."));
+        this.add(new Supporter("IPhonelover XD"));
+        this.add(new Supporter("technodwarf", "https://www.youtube.com/@technodwarf"));
+        this.add(new Supporter("Klalorathy", "..."));
+        this.add(new Supporter("Pexiaus"));
+        this.add(new Supporter("The Anime Protagonist", "https://www.youtube.com/@DuhAnimeProtagonist/featured"));
+        this.add(new Supporter("wangyupu"));
+        this.add(new Supporter("Wone_J", "https://space.bilibili.com/13505331"));
+        this.add(new Supporter("资 云"));
+        this.add(new Supporter("dustink", "https://space.bilibili.com/514637376"));
+        this.add(new Supporter("Usaky舞麒", "https://space.bilibili.com/673732587", "..."));
+        this.add(new Supporter("流风", "...", "..."));
+        this.add(new Supporter("南风不知我意"));
+        this.add(new Supporter("WATERMELON"));
+        this.add(new Supporter("Tanis Gon"));
+        this.add(new Supporter("临夜", "https://space.bilibili.com/1544935486"));
+        this.add(new Supporter("Sundown Story"));
+        this.add(new Supporter("Michael Creeper_", "...", "..."));
+        this.add(new Supporter("Hrymka", "...", "..."));
+        this.add(new Supporter("Rasty", "https://www.youtube.com/@RastyStudio"));
+        this.add(new Supporter("Clesten", "https://www.youtube.com/@clesten9475"));
+        this.add(new Supporter("Wenbai", "https://space.bilibili.com/697902619", "..."));
+        this.add(new Supporter("骚老师的MC动画"));
+        this.add(new Supporter("Jackey_lin", "..."));
+        this.add(new Supporter("snifferpond"));
+        this.add(new Supporter("爱发电用户_e9eaf^^^"));
+        this.add(new Supporter("爱发电用户_10b4f^^^"));
+        this.add(new Supporter("爱发电用户_cfaea^^^", "..."));
+        this.add(new Supporter("HeimNad"));
+        this.add(new Supporter("Minecraft地形师茶馆", "https://space.bilibili.com/3461578447915295"));
+        this.add(new Supporter("Jmol", "https://space.bilibili.com/108360657"));
+        this.add(new Supporter("小博ace"));
+        this.add(new Supporter("Michql"));
+        this.add(new Supporter("ESAT"));
+        this.add(new Supporter("愉悦的hehe11", "...", "..."));
+    }
 
-        for (BaseType s : supporters)
-        {
-            MapType supporter = s.asMap();
-
-            this.supporters.add(new Supporter(
-                supporter.getString("name"),
-                supporter.getString("link"),
-                LinkUtils.create(supporter.get("banner"))
-            ));
-        }
+    private void add(Supporter supporter)
+    {
+        this.supporters.add(supporter);
     }
 
     public List<Supporter> getBBSEarlyAccessSupporters()
