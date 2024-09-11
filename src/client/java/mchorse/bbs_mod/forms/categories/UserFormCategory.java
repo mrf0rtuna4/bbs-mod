2f9e85e579cba480fa66a5c8cdcecf63bca1f942
McHorse <mchorselessrider@gmail.com>
2024-09-11 18:34:40 +0100

    Allow editing forms in user categories

--- src/client/java/mchorse/bbs_mod/forms/categories/UserFormCategory.java
+++ src/client/java/mchorse/bbs_mod/forms/categories/UserFormCategory.java
@@ -1,5 +1,6 @@
 package mchorse.bbs_mod.forms.categories;
 
+import mchorse.bbs_mod.forms.forms.Form;
 import mchorse.bbs_mod.l10n.keys.IKey;
 import mchorse.bbs_mod.ui.forms.UIFormList;
 import mchorse.bbs_mod.ui.forms.categories.UIFormCategory;
@@ -10,6 +11,12 @@ public class UserFormCategory extends FormCategory
     public UserFormCategory(IKey title)
     {
         super(title);
+    }
+
+    @Override
+    public boolean canModify(Form form)
+    {
+        return true;
     }
 
     @Override
