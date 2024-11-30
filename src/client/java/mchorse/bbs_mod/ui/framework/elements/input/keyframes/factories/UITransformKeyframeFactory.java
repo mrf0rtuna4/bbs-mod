cc541f23f13065e5b494c8821cf4f584df672063
McHorse <mchorselessrider@gmail.com>
2024-11-30 10:02:41 +0000

    Apply changes to selected transform keyframes

--- src/client/java/mchorse/bbs_mod/ui/framework/elements/input/keyframes/factories/UITransformKeyframeFactory.java
+++ src/client/java/mchorse/bbs_mod/ui/framework/elements/input/keyframes/factories/UITransformKeyframeFactory.java
@@ -1,9 +1,13 @@
 package mchorse.bbs_mod.ui.framework.elements.input.keyframes.factories;
 
 import mchorse.bbs_mod.ui.framework.elements.input.UIPropTransform;
+import mchorse.bbs_mod.ui.framework.elements.input.keyframes.UIKeyframeSheet;
 import mchorse.bbs_mod.ui.framework.elements.input.keyframes.UIKeyframes;
+import mchorse.bbs_mod.utils.MathUtils;
 import mchorse.bbs_mod.utils.keyframes.Keyframe;
 import mchorse.bbs_mod.utils.pose.Transform;
+
+import java.util.function.Consumer;
 
 public class UITransformKeyframeFactory extends UIKeyframeFactory<Transform>
 {
@@ -13,10 +17,101 @@ public class UITransformKeyframeFactory extends UIKeyframeFactory<Transform>
     {
         super(keyframe, editor);
 
-        this.transform = new UIPropTransform(keyframe::setValue);
+        this.transform = new UIPoseTransforms(this);
         this.transform.enableHotkeys();
         this.transform.setTransform(keyframe.getValue());
 
         this.scroll.add(this.transform);
+    }
+
+    public static class UIPoseTransforms extends UIPropTransform
+    {
+        private UITransformKeyframeFactory editor;
+
+        public UIPoseTransforms(UITransformKeyframeFactory editor)
+        {
+            this.editor = editor;
+        }
+
+        public static void apply(UIKeyframes editor, Keyframe keyframe, Consumer<Transform> consumer)
+        {
+            for (UIKeyframeSheet sheet : editor.getGraph().getSheets())
+            {
+                if (sheet.channel.getFactory() != keyframe.getFactory())
+                {
+                    continue;
+                }
+
+                for (Keyframe kf : sheet.selection.getSelected())
+                {
+                    if (kf.getValue() instanceof Transform transform)
+                    {
+                        kf.preNotifyParent();
+                        consumer.accept(transform);
+                        kf.postNotifyParent();
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void setT(double x, double y, double z)
+        {
+            float dx = (float) (x - this.getTransform().translate.x);
+            float dy = (float) (y - this.getTransform().translate.y);
+            float dz = (float) (z - this.getTransform().translate.z);
+
+            apply(this.editor.editor, this.editor.keyframe, (poseT) ->
+            {
+                poseT.translate.x += dx;
+                poseT.translate.y += dy;
+                poseT.translate.z += dz;
+            });
+        }
+
+        @Override
+        public void setS(double x, double y, double z)
+        {
+            float dx = (float) (x - this.getTransform().scale.x);
+            float dy = (float) (y - this.getTransform().scale.y);
+            float dz = (float) (z - this.getTransform().scale.z);
+
+            apply(this.editor.editor, this.editor.keyframe, (poseT) ->
+            {
+                poseT.scale.x += dx;
+                poseT.scale.y += dy;
+                poseT.scale.z += dz;
+            });
+        }
+
+        @Override
+        public void setR(double x, double y, double z)
+        {
+            float dx = MathUtils.toRad((float) x) - this.getTransform().rotate.x;
+            float dy = MathUtils.toRad((float) y) - this.getTransform().rotate.y;
+            float dz = MathUtils.toRad((float) z) - this.getTransform().rotate.z;
+
+            apply(this.editor.editor, this.editor.keyframe, (poseT) ->
+            {
+                poseT.rotate.x += dx;
+                poseT.rotate.y += dy;
+                poseT.rotate.z += dz;
+            });
+        }
+
+        @Override
+        public void setR2(double x, double y, double z)
+        {
+            float dx = MathUtils.toRad((float) x) - this.getTransform().rotate2.x;
+            float dy = MathUtils.toRad((float) y) - this.getTransform().rotate2.y;
+            float dz = MathUtils.toRad((float) z) - this.getTransform().rotate2.z;
+
+            apply(this.editor.editor, this.editor.keyframe, (poseT) ->
+            {
+                poseT.rotate2.x += dx;
+                poseT.rotate2.y += dy;
+                poseT.rotate2.z += dz;
+            });
+        }
     }
 }
