28fc16b0fc3f66825432069daf6585a30aea0349
Chryfi <71967555+Chryfi@users.noreply.github.com>
2024-12-01 14:43:11 +0100

    doc: more docs for MatrixUtils

--- src/main/java/mchorse/bbs_mod/utils/MatrixUtils.java
+++ src/main/java/mchorse/bbs_mod/utils/MatrixUtils.java
@@ -1,5 +1,6 @@
 package mchorse.bbs_mod.utils;
 
+import org.jetbrains.annotations.Nullable;
 import org.joml.*;
 
 import java.lang.Math;
@@ -42,6 +43,12 @@ public class MatrixUtils {
             this.index = this.name().toUpperCase().charAt(0) - 'X';
         }
 
+        /**
+         * @param vecIndex
+         * @return the corresponding axis that represents the vector component specified by the index
+         *         e.g. index = 0 is axis X, index = 2 is axis Z and index > 2 is null.
+         */
+        @Nullable
         public static Axis axisOfVectorIndex(int vecIndex) {
             return switch (vecIndex) {
                 case 0 -> X;
@@ -51,11 +58,21 @@ public class MatrixUtils {
             };
         }
 
+        /**
+         * @param radians angle in radians around this axis
+         * @return the rotation around this axis
+         */
         public Matrix3d getRotationMatrix(double radians)
         {
             return this.getRotationMatrix(new Matrix3d(), radians);
         }
 
+        /**
+         *
+         * @param mat the matrix to set the result to. The original matrix is overwritten.
+         * @param radians angle in radians around this axis
+         * @return the rotation around this axis
+         */
         public Matrix3d getRotationMatrix(Matrix3d mat, double radians)
         {
             mat.identity();
@@ -73,6 +90,9 @@ public class MatrixUtils {
             }
         }
 
+        /**
+         * @return the unit vector representing this axis.
+         */
         public Vector3d getAxisVector() {
             return new Vector3d(0, 0, 0).setComponent(this.index, 1);
         }
@@ -88,7 +108,10 @@ public class MatrixUtils {
         }
 
         /**
+         * Calculates the angle around this axis needed to achieve the given rotation
+         * matrix.
          * @param forwardAxis the forward axis to test for flips and rotation
+         * @param rotation the rotation matrix to calculate the angle from
          * @return angle in radians around this axis
          */
         public double getAngleAround(Axis forwardAxis, Matrix3d rotation) {
@@ -157,10 +180,9 @@ public class MatrixUtils {
 
         /**
          * Orders the given angles that correspond to this rotation order back to a XYZ order.
-         * @param angle0
-         * @param angle1
-         * @param angle2
-         * @return
+         * @param angle0 around first rotation axis
+         * @param angle1 around second rotation axis
+         * @param angle2 around third rotation axis
          */
         public Vector3d orderAnglesToXYZ(double angle0, double angle1, double angle2) {
             Vector3d angles = new Vector3d();
@@ -170,31 +192,22 @@ public class MatrixUtils {
             return angles;
         }
 
+        /**
+         *
+         * @param axis the axis around which an angle should be calculated
+         * @return the forward axis, orthogonal to the given axis, that is used for testing flips.
+         */
         public Axis getForwardForRotationAxis(Axis axis) {
             /* +z and +y are nice forwards, is more standard */
             return axis == Axis.Z ? Axis.Y : Axis.Z;
         }
 
         /**
+         * Calculates the euler angles needed to achieve the given transformation matrix with this rotation order.
          * @param transform
          * @return euler xyz angles in radians
          */
         public Vector3d getEulerAngles(Matrix3d transform) {
-            /* copy since we will modify this in the process */
-            transform = new Matrix3d(transform);
-
-            double angle0 = this.extractAngleAroundAxis(this.rotAxis0, transform);
-            double angle1 = this.extractAngleAroundAxis(this.rotAxis1, transform);
-            double angle2 = this.extractAngleAroundAxis(this.rotAxis2, transform);
-
-            return this.orderAnglesToXYZ(angle0, angle1, angle2);
-        }
-
-        /**
-         * @param transform
-         * @return euler xyz angles in radians
-         */
-        public Vector3d getEulerAngles(Matrix3d transform, Matrix3d transformAxisSpace) {
             /* copy since we will modify this in the process */
             transform = new Matrix3d(transform);
 
